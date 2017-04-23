5a94e768d7308a38b32e5746ca33fa5a423f34a1
Lynn Chen <lynn.d.chen@gmail.com>
2017-04-22 22:59:25 -0700

    fix tabbing in carousel.js

--- Public/JS/carousel.js
+++ Public/JS/carousel.js
@@ -4,16 +4,16 @@ let slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-    showDivs(slideIndex += n);
+  showDivs(slideIndex += n);
 }
 
 function showDivs(n) {
-    var i;
-    var $projects = $("figure.project");
-    if (n > $projects.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = $projects.length} ;
-    for (i = 0; i < $projects.length; i++) {
-        $projects[i].style.display = "none";
-    }
-    $projects[slideIndex-1].style.display = "inline-block";
+  var i;
+  var $projects = $("figure.project");
+  if (n > $projects.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = $projects.length}
+  for (i = 0; i < $projects.length; i++) {
+    $projects[i].style.display = "none";
+  }
+  $projects[slideIndex-1].style.display = "inline-block";
 }
