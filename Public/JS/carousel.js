b9d716ece855d1c1517331b18f68f289d81f9954
Lynn Chen <lynn.d.chen@gmail.com>
2017-04-26 09:30:00 -0700

    added skill data and wrapped carousel.js in module

--- Public/JS/carousel.js
+++ Public/JS/carousel.js
@@ -1,21 +1,23 @@
 'use strict';
-let slideIndex = 0;
+(function(module) {
+  let slideIndex = 0;
 
-function showProject(n) {
-  let $projects = $('figure.project');
-  $projects.hide();
+  function showProject(n) {
+    let $projects = $('figure.project');
+    $projects.hide();
 
-  if (n >= $projects.length) {
-    slideIndex = 0;
-  } else if (n < 0) {
-    slideIndex = $projects.length - 1;
+    if (n >= $projects.length) {
+      slideIndex = 0;
+    } else if (n < 0) {
+      slideIndex = $projects.length - 1;
+    }
+
+    $($projects[slideIndex]).show();
   }
 
-  $($projects[slideIndex]).show();
-}
+  showProject(slideIndex);
 
-showProject(slideIndex);
-
-function incProject(n) {
-  showProject(slideIndex += n);
-}
+  function incProject(n) {
+    showProject(slideIndex += n);
+  }
+}) (window);
