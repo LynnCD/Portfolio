88069e25af65cbd354406cb9f43448efae09ae51
Lynn Chen <lynn.d.chen@gmail.com>
2017-05-01 14:26:24 -0700

    get repos on skill.js, added map()

--- Public/JS/MODELS/repo.js
+++ Public/JS/MODELS/repo.js
@@ -4,10 +4,36 @@
   const repos = {};
   repos.all = [];
   repos.requestRepos = callback => {
-    $.get(`/github/user/repos`)
+    $.get(`/github/users/LynnDC/repos`)
     .then(res => repos.all = res, err => console.error(err))
     .then(callback);
   }
+
+  // TODO start block
+  // let token = '04ebaa69df7ee3d77e79fc46b1978c25d610c5c1';
+  // let username = 'LynnDC';
+
+  // repos.requestRepos = function(callback) {
+  //   // : How would you like to fetch your repos? Don't forget to call the callback.
+  //   //       Remember that the callback function we'll want to call relies on repos.all
+  //   //       being an array with a bunch of repo objects in it, so you'll need to
+  //   //       populate it with the response from Github before you call the callback.
+  //   $.ajax({
+  //     url: `https://api.github.com/users/${username}/repos`,
+  //     method: 'GET',
+  //     header: {
+  //       Authorization: `token ${token}`
+  //     }
+  //   })
+  //    .then(
+  //      function(response) {
+  //        repos.all = response;
+  //        callback();
+  //      }
+  //    )
+  // };
+  // TODO end block
+
   repos.with = attr => repos.all.filter(repo => repo[attr]);
   module.repos = repos;
 })(window);
--- server.js
+++ server.js
@@ -1,8 +1,4 @@
 'use strict';
-// get: is to get the thing thats already exsit, if no data exist yet, retrieve nothing.
-//post: create a resoure in server and send new data to the data base.
-//put: update exsiting entry in the existing data base, it can only update things thats already exsit. can be used without 'get' if you konw the exact ID.
-//delete: removes an existing entry from the database.
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -13,14 +9,16 @@ app.use(express.static('./public'));
 
 function proxyGitHub(request, response) {
   console.log('Routing GitHub request for', request.params[0]);
+  console.log(`token: ${process.env.GITHUB_TOKEN}`);
   (requestProxy({
     url: `https://api.github.com/${request.params[0]}`,
     headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
   }))(request, response);
 }
 
-app.get('/github/*', proxyGitHub);
 
+app.get('/skills', (request, response) => response.sendFile('portfolio.html', {root: './public'}));
+app.get('/github/*', proxyGitHub);
 app.get('*', (request, response) => response.sendFile('portfolio.html', {root: './public'}));
 
 app.post('/project_data', bodyParser, (request, response)=>
